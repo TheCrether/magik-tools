61c6534c3067489621e428082850c15c746b0635
TheCrether <thecrether@gmail.com>
2024-05-22 08:31:25 +0200

    GIS-305: disable VariableNamingCheck by default also add better default ignores to variable naming check if it is enabled

--- magik-checks/src/main/java/nl/ramsolutions/sw/magik/checks/checks/VariableNamingCheck.java
+++ magik-checks/src/main/java/nl/ramsolutions/sw/magik/checks/checks/VariableNamingCheck.java
@@ -6,11 +6,13 @@ import nl.ramsolutions.sw.magik.MagikFile;
 import nl.ramsolutions.sw.magik.analysis.scope.GlobalScope;
 import nl.ramsolutions.sw.magik.analysis.scope.Scope;
 import nl.ramsolutions.sw.magik.analysis.scope.ScopeEntry;
+import nl.ramsolutions.sw.magik.checks.DisabledByDefault;
 import nl.ramsolutions.sw.magik.checks.MagikCheck;
 import org.sonar.check.Rule;
 import org.sonar.check.RuleProperty;
 
 /** Check for valid variable names. */
+@DisabledByDefault
 @Rule(key = VariableNamingCheck.CHECK_KEY)
 public class VariableNamingCheck extends MagikCheck {
 
@@ -18,7 +20,7 @@ public class VariableNamingCheck extends MagikCheck {
   public static final String CHECK_KEY = "VariableNaming";
 
   private static final String MESSAGE = "Give the variable \"%s\" a proper descriptive name.";
-  private static final String DEFAULT_WHITELIST = "x,y,z";
+  private static final String DEFAULT_WHITELIST = "x,y,z,i,j,k";
   private static final int DEFAULT_MIN_LENGTH = 3;
 
   /** Whitelist (comma separated) of variable names to allow/ignore. */
