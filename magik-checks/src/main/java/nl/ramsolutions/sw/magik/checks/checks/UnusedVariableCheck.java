5b8121446539e457f00ace63ebda972c0614bcae
TheCrether <thecrether@gmail.com>
2024-07-11 13:24:38 +0200

    GIS-330: add option for checking unused parameters

--- magik-checks/src/main/java/nl/ramsolutions/sw/magik/checks/checks/UnusedVariableCheck.java
+++ magik-checks/src/main/java/nl/ramsolutions/sw/magik/checks/checks/UnusedVariableCheck.java
@@ -1,11 +1,9 @@
 package nl.ramsolutions.sw.magik.checks.checks;
 
 import com.sonar.sslr.api.AstNode;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-
 import nl.ramsolutions.sw.magik.analysis.AstQuery;
 import nl.ramsolutions.sw.magik.analysis.scope.GlobalScope;
 import nl.ramsolutions.sw.magik.analysis.scope.Scope;
@@ -14,27 +12,26 @@ import nl.ramsolutions.sw.magik.api.MagikGrammar;
 import nl.ramsolutions.sw.magik.api.MagikOperator;
 import nl.ramsolutions.sw.magik.checks.MagikCheck;
 import org.sonar.check.Rule;
+import org.sonar.check.RuleProperty;
 
-/**
- * Check for unused variables.
- */
+/** Check for unused variables. */
 @Rule(key = UnusedVariableCheck.CHECK_KEY)
 public class UnusedVariableCheck extends MagikCheck {
 
   @SuppressWarnings("checkstyle:JavadocVariable")
   public static final String CHECK_KEY = "UnusedVariable";
 
+  private static final boolean DEFAULT_CHECK_PARAMETERS = false;
   private static final String MESSAGE = "Remove the unused local variable \"%s\".";
+  private static final String PARAM_MESSAGE = "Remove the unused parameter \"%s\".";
 
-  private boolean checkParameters;
-
-  public UnusedVariableCheck() {
-    this.checkParameters = false;
-  }
-
-  public UnusedVariableCheck(final boolean checkParameters) {
-    this.checkParameters = checkParameters;
-  }
+  @RuleProperty(
+      key = "check parameters",
+      defaultValue = "" + DEFAULT_CHECK_PARAMETERS,
+      description = "Check for unused parameters",
+      type = "BOOLEAN")
+  @SuppressWarnings("checkstyle:VisibilityModifier")
+  public boolean checkParameters = DEFAULT_CHECK_PARAMETERS;
 
   private boolean isAssignedToDirectly(final AstNode identifierNode) {
     final AstNode variableDefinitionNode = identifierNode.getParent();
@@ -47,23 +44,22 @@ public class UnusedVariableCheck extends MagikCheck {
     }
 
     return variableDefinitionNode.getTokens().stream()
-      .anyMatch(
-        token ->
-          token.getValue().equals(MagikOperator.CHEVRON.getValue())
-            || token.getValue().equals(MagikOperator.BOOT_CHEVRON.getValue()));
+        .anyMatch(
+            token ->
+                token.getValue().equals(MagikOperator.CHEVRON.getValue())
+                    || token.getValue().equals(MagikOperator.BOOT_CHEVRON.getValue()));
   }
 
   private boolean isPartOfMultiVariableDefinition(final AstNode identifierNode) {
     // Either part of a VARIABLE_DEFINITION_MULTI or FOR_VARIABLES.
     final AstNode variableDefMultiNode =
-      AstQuery.getParentFromChain(
-        identifierNode,
-        MagikGrammar.IDENTIFIERS_WITH_GATHER,
-        MagikGrammar.VARIABLE_DEFINITION_MULTI
-      );
+        AstQuery.getParentFromChain(
+            identifierNode,
+            MagikGrammar.IDENTIFIERS_WITH_GATHER,
+            MagikGrammar.VARIABLE_DEFINITION_MULTI);
     final AstNode forVariablesNode =
-      AstQuery.getParentFromChain(
-        identifierNode, MagikGrammar.IDENTIFIERS_WITH_GATHER, MagikGrammar.FOR_VARIABLES);
+        AstQuery.getParentFromChain(
+            identifierNode, MagikGrammar.IDENTIFIERS_WITH_GATHER, MagikGrammar.FOR_VARIABLES);
     return variableDefMultiNode != null || forVariablesNode != null;
   }
 
@@ -80,7 +76,7 @@ public class UnusedVariableCheck extends MagikCheck {
 
     final AstNode assignablesNode = expressionNode.getParent();
     return assignablesNode != null
-      && assignablesNode.is(MagikGrammar.MULTIPLE_ASSIGNMENT_ASSIGNABLES);
+        && assignablesNode.is(MagikGrammar.MULTIPLE_ASSIGNMENT_ASSIGNABLES);
   }
 
   private boolean anyNextSiblingUsed(final AstNode identifierNode) {
@@ -95,7 +91,9 @@ public class UnusedVariableCheck extends MagikCheck {
       }
 
       final ScopeEntry siblingEntry = scope.getScopeEntry(sibling);
-      if (siblingEntry != null && sibling.getTokenValue().equals(identifierNode.getTokenValue()) && !siblingEntry.getUsages().isEmpty()) {
+      if (siblingEntry != null
+          && sibling.getTokenValue().equals(identifierNode.getTokenValue())
+          && !siblingEntry.getUsages().isEmpty()) {
         return true;
       }
 
@@ -116,9 +114,11 @@ public class UnusedVariableCheck extends MagikCheck {
     // - but this one is not
     for (final ScopeEntry entry : List.copyOf(scopeEntries)) {
       final AstNode entryNode = entry.getDefinitionNode();
-//      if (this.isPartOfMultiVariableDefinition(entryNode) && this.anyNextSiblingUsed(entryNode)
-//          || this.isPartOfMultiAssignment(entryNode)) {
-      if (this.isPartOfMultiVariableDefinition(entryNode) || this.isPartOfMultiAssignment(entryNode)) {
+      //      if (this.isPartOfMultiVariableDefinition(entryNode) &&
+      // this.anyNextSiblingUsed(entryNode)
+      //          || this.isPartOfMultiAssignment(entryNode)) {
+      if (this.isPartOfMultiVariableDefinition(entryNode)
+          || this.isPartOfMultiAssignment(entryNode)) {
         scopeEntries.remove(entry);
       }
     }
@@ -131,7 +131,12 @@ public class UnusedVariableCheck extends MagikCheck {
 
       final AstNode entryNode = entry.getDefinitionNode();
       final String name = entryNode.getTokenValue();
-      final String message = String.format(MESSAGE, name);
+      String message = String.format(MESSAGE, name);
+
+      if (entry.isType(ScopeEntry.Type.PARAMETER)) {
+        message = String.format(PARAM_MESSAGE, name);
+      }
+
       this.addIssue(entryNode, message);
     }
   }
@@ -145,7 +150,7 @@ public class UnusedVariableCheck extends MagikCheck {
 
         // But not globals/dynamics which are assigned to directly
         if ((scopeEntry.isType(ScopeEntry.Type.GLOBAL, ScopeEntry.Type.DYNAMIC))
-          && this.isAssignedToDirectly(scopeEntryNode)) {
+            && this.isAssignedToDirectly(scopeEntryNode)) {
           continue;
         }
 
